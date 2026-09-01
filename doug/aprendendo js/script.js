26d8004e12dec0becf02e42b315dd6b261977e0d
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2026-08-31 21:20:37 -0300

    doug: iniciando sobre variaveis

--- doug/aprendendo js/script.js
+++ doug/aprendendo js/script.js
@@ -1,3 +1,22 @@
 // primeiro hello world 
 console.log("Hello World !")
 
+// ------------- USANDO VARIAVEIS ----------------
+
+/*
+ var = originalmente usado para declarar as variaveis  , mas tem um problema o usa atualmente 
+        ela permite a redeclaração da variavel podendo causar erro no codigo 
+ let = usada atualmente para declarar variaveis , por ser mais atual , ela impede declarar a 
+        variavel mais de uma vez 
+*/
+var nome = "doug";
+let sobrenome = "vikt";
+
+console.log(nome ,sobrenome);
+
+// alterando os valores das variaveis
+
+let preco = 120;
+console.log("preço original :",preco);
+preco = 120 - 40;
+console.log("retirou 40 :",preco);
